d19967e58ffcecfb3b8fa0b8c16e89efacb044c5
Michael Burridge <mburridge@zyriab.co.uk>
2020-07-23 17:38:55 +0100

    adds contact-form demo

---
+++ contact-form/frontity.settings.js
@@ -0,0 +1,53 @@
+const settings = {
+  "name": "contact-form",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "About",
+              "/about/"
+            ],
+           /* [
+              "Contact",
+              "/contact/"
+            ], */
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "http://brochure.test/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+    /**
+     * add the CF7 package to the packages array
+     */
+    "frontity-contact-form-7"
+  ]
+};
+
+export default settings;
