2d987da36e7a29c588e9067037c978b966a55b9f
Michael Burridge <mburridge@zyriab.co.uk>
2021-02-09 15:23:29 +0000

    Changes indentation in 'class-component.js'

--- demo-class-component/packages/mars-theme/src/components/class-component.js
+++ demo-class-component/packages/mars-theme/src/components/class-component.js
@@ -3,21 +3,21 @@ import { connect, styled } from 'frontity'
 
 class ClassComponent extends React.Component {
   render() {
-		return (
-			<Container>
-				<h2>This page is rendered by a class-based component</h2>
-				<p><em>Current path:</em> {this.props.state.router.link}</p>
-				<p><em>Value of 'state.theme.ccString':</em> {this.props.state.theme.ccString}</p>
-			</Container>
-		)
+    return (
+      <Container>
+        <h2>This page is rendered by a class-based component</h2>
+        <p><em>Current path:</em> {this.props.state.router.link}</p>
+        <p><em>Value of 'state.theme.ccString':</em> {this.props.state.theme.ccString}</p>
+      </Container>
+    )
   }
 }
 
 export default connect(ClassComponent)
 
 const Container = styled.div`
-	h2, p {
-		display: block;
-		width: 100%;
-	}
+  h2, p {
+    display: block;
+    width: 100%;
+  }
 `
