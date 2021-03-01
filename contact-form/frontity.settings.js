780ce8f567de80c2be5e0bb3d539d56f611cfe4f
Michael Burridge <mburridge@zyriab.co.uk>
2021-03-01 10:56:48 +0000

    Updates to use publicly available source at test.frontity.org

--- contact-form/frontity.settings.js
+++ contact-form/frontity.settings.js
@@ -1,53 +1,47 @@
 const settings = {
-  "name": "contact-form",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: "contact-form",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "About",
-              "/about/"
-            ],
-           /* [
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["About", "/about/"],
+            /* [
               "Contact",
               "/contact/"
             ], */
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "http://brochure.test/wp-json"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "http://test.frontity.org/wp-json",
+        },
+      },
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
     /**
      * add the CF7 package to the packages array
      */
-    "frontity-contact-form-7"
-  ]
+    "frontity-contact-form-7",
+  ],
 };
 
 export default settings;
--- contact-form/packages/mars-theme/src/components/footer.js
+++ contact-form/packages/mars-theme/src/components/footer.js
@@ -7,30 +7,29 @@ import React from "react";
 import { connect, styled } from "frontity";
 
 const Footer = ({ state, libraries }) => {
+  const data = state.source.get("/contact-form");
+  const contactForm = state.source.page[data.id];
+  const Html2React = libraries.html2react.Component;
 
-    const data = state.source.get("/contact");
-    const contactForm = state.source.page[data.id];
-    const Html2React = libraries.html2react.Component;
-
-    return (
-      <>
-        <ContactForm>
-            <Html2React html={contactForm.content.rendered} />
-        </ContactForm>
-      </>
-    );
+  return (
+    <>
+      <ContactForm>
+        <Html2React html={contactForm.content.rendered} />
+      </ContactForm>
+    </>
+  );
 };
 
 export default connect(Footer);
 
 const ContactForm = styled.div`
-    width: 848px;
-    max-width: 100%;
-    box-sizing: border-box;
-    padding: 24px;
-    color: #fff;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    font-size: 0.8em;
+  width: 848px;
+  max-width: 100%;
+  box-sizing: border-box;
+  padding: 24px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-size: 0.8em;
 `;
